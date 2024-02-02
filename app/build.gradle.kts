3444bb0d9f0b9aed48e1fb40ded041db51957e4f
sungyo <saponin0421@gmail>
2024-02-02 16:31:01 +0900

    2월2일 2월 2일

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,9 @@ android {
 dependencies {
     implementation("androidx.navigation:navigation-fragment:2.7.6")
     implementation("androidx.navigation:navigation-ui:2.7.6")
+    implementation("androidx.preference:preference:1.2.1")
+    implementation("androidx.core:core:1.12.0")
+    implementation("com.google.android.gms:play-services-ads:22.6.0")
     val workVersion = "2.9.0"
 
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -50,4 +53,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
 }
