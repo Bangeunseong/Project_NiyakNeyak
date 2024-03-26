8d1a0a1035bc28bacb9085ce4d277ac9e4bd1508
sungyo <saponin0421@gmail.com>
2024-03-26 15:06:40 +0900

    3.26 파일추가

---
+++ app/src/main/java/com/capstone/project_niyakneyak/ui/main/activity/PracticeActivity.java
@@ -0,0 +1,16 @@
+package com.capstone.project_niyakneyak.ui.main.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.capstone.project_niyakneyak.R;
+
+public class PracticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice);
+    }
+}
