e731e78238a2e0964e086a1116830c02afd3c9a5
Bangeunseong <bangeunseong@naver.com>
2024-03-28 16:29:12 +0900

    Committed

--- app/src/main/java/com/capstone/project_niyakneyak/main/etc/Filters.kt
+++ app/src/main/java/com/capstone/project_niyakneyak/main/etc/Filters.kt
@@ -1,5 +1,6 @@
 package com.capstone.project_niyakneyak.main.etc
 
+import android.text.TextUtils
 import com.google.firebase.firestore.Query
 
 class Filters {
@@ -8,4 +9,22 @@ class Filters {
     var endDate: String? = null
     var sortBy: String? = null
     var sortDirection: Query.Direction = Query.Direction.DESCENDING
+
+    fun hasCategory(): Boolean{
+        return !TextUtils.isEmpty(category)
+    }
+
+    fun hasStartDate(): Boolean{
+        return !TextUtils.isEmpty(startDate)
+    }
+
+    fun hasEndDate(): Boolean{
+        return !TextUtils.isEmpty(endDate)
+    }
+
+    fun hasSortBy(): Boolean{
+        return !TextUtils.isEmpty(sortBy)
+    }
+
+
 }
