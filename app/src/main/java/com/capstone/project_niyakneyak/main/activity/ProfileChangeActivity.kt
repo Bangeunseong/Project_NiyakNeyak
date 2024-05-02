3aece1618b3058f69e4a8d09132067fdfee654ef
sungyo <saponin0421@gmail.com>
2024-05-02 18:20:58 +0900

    5.2 프로필 이미지

--- app/src/main/java/com/capstone/project_niyakneyak/main/activity/ProfileChangeActivity.kt
+++ app/src/main/java/com/capstone/project_niyakneyak/main/activity/ProfileChangeActivity.kt
@@ -8,30 +8,21 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import com.capstone.project_niyakneyak.R
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.capstone.project_niyakneyak.main.adapter.AvatarAdapter
 
 class ProfileChangeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile_change)
-        val avatarImages = listOf(R.drawable.boy, R.drawable.girl, R.drawable.man, R.drawable.woman)
-        val avatarAdapter = AvatarAdapter(avatarImages)
 
-        val avatarList = findViewById<RecyclerView>(R.id.avatar_list)
-        avatarList.layoutManager = GridLayoutManager(this, 3) // Change this line
-        avatarList.adapter = avatarAdapter
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val setprofileButton = findViewById<Button>(R.id.set_profile_button)
+        setprofileButton.setOnClickListener {
+            // Set selectedImage as profile image
         }
-        val selectButton = findViewById<Button>(R.id.select_from_gallery_button)
-        selectButton.setOnClickListener {
-            val selectedImage = avatarImages[avatarAdapter.selectedPosition]
+        val galleryButton = findViewById<Button>(R.id.select_from_gallery_button)
+        galleryButton.setOnClickListener {
             // Set selectedImage as profile image
         }
         val backButton = findViewById<Button>(R.id.back_button)
