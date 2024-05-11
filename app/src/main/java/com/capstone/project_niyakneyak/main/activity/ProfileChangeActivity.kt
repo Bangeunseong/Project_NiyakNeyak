152b2090b4119667f84fc1e2f1501e4585bb5b24
sungyo <saponin0421@gmail.com>
2024-05-11 16:47:01 +0900

    5월 11일 이게 최적화됨

--- app/src/main/java/com/capstone/project_niyakneyak/main/activity/ProfileChangeActivity.kt
+++ app/src/main/java/com/capstone/project_niyakneyak/main/activity/ProfileChangeActivity.kt
@@ -1,22 +1,22 @@
 package com.capstone.project_niyakneyak.main.activity
+import android.Manifest
 import android.content.Intent
-import android.graphics.Color
+import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AlertDialog
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.capstone.project_niyakneyak.R
-import androidx.recyclerview.widget.RecyclerView
 import com.capstone.project_niyakneyak.databinding.ActivityProfileChangeBinding
-import com.capstone.project_niyakneyak.login.activity.LoginActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -32,10 +32,15 @@ class ProfileChangeActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private val uiScope = CoroutineScope(Dispatchers.Main)
 
+    lateinit var photoLauncher: ActivityResultLauncher<Intent>
+
+
+
     private var _binding: ActivityProfileChangeBinding? = null
     private val binding get() = _binding!!
     private var selectedImageView: ImageView? = null
     private var defaultDrawable: Drawable? = null
+    var userId: String? = null
 
     val TAG = "ProfileChangeActivity"
 
@@ -48,25 +53,25 @@ class ProfileChangeActivity : AppCompatActivity() {
         firestore = Firebase.firestore // Firestore 초기화
         user = auth.currentUser // 현재 사용자 가져오기
 
-        val userId = auth.currentUser?.uid
+        userId = auth.currentUser?.uid
         if (userId != null) {
-            val userDocument = firestore.collection("users").document(userId)
+            val userDocument = firestore.collection("users").document(userId!!)
             userDocument.get()
-            firestore.collection("users").document(userId).get()
+            firestore.collection("users").document(userId!!).get()
                 .addOnSuccessListener { document ->
                     if (document != null) {
                         Log.d(TAG, "DocumentSnapshot data: ${document.data}")
-                        if (document.contains("프로필이미지")) {
+                        if (document.contains("profilePic")) {
                             // "프로필이미지" 필드가 이미 존재하는 경우의 처리
-                            Log.d(TAG, "프로필이미지 field already exists!")
+                            Log.d(TAG, "profilePid field already exists!")
                         } else {
                             // "프로필이미지" 필드가 존재하지 않는 경우, 필드를 추가
-                            userDocument.update("프로필이미지", "default_profile_image_url")
+                            userDocument.update("profilePic", "default_profile_image_url")
                                 .addOnSuccessListener {
-                                    Log.d(TAG, "프로필이미지 field successfully added!")
+                                    Log.d(TAG, "profilePic field successfully added!")
                                 }
                                 .addOnFailureListener { e ->
-                                    Log.w(TAG, "Error adding 프로필이미지 field", e)
+                                    Log.w(TAG, "Error adding profilePic field", e)
                                 }
                         }
                     } else {
@@ -81,10 +86,13 @@ class ProfileChangeActivity : AppCompatActivity() {
         }
 
 
+
+
         binding.imageViewBoy.setOnClickListener {
 
             Toast.makeText(this, "boy.", Toast.LENGTH_SHORT).show()
             binding.imageViewYou.setImageResource(R.drawable.boy)
+
 
         }
         binding.imageViewGirl.setOnClickListener {
@@ -100,24 +108,91 @@ class ProfileChangeActivity : AppCompatActivity() {
             Toast.makeText(this, "woman.", Toast.LENGTH_SHORT).show()
             binding.imageViewYou.setImageResource(R.drawable.woman)
         }
-        binding.setProfileButton.setOnClickListener {
-            selectedImageView= binding.imageViewYou
-            // Set selectedImage as profile image
-            if (selectedImageView == null) {
-                Toast.makeText(this, "프로필 이미지를 선택해주세요.", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }else{
+
+        photoLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                //
+                selectedImageView = binding.imageViewYou
+                uploadToFirestore(result.data?.data!!)
 
             }
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+        }
+
+
+        binding.setProfileButton.setOnClickListener {
+
         }
         binding.backButton.setOnClickListener {
             finish()
         }
         binding.selectFromGalleryButton.setOnClickListener {
-            // Set selectedImage as profile image
+            //Toast.makeText(this, "a.", Toast.LENGTH_SHORT).show()
+            checkPermissionAndPickPhoto()
+
 
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 100)
+        }
+    }
+    fun checkPermissionAndPickPhoto() {
+        //Toast.makeText(this, "b.", Toast.LENGTH_SHORT).show()
+        var readExternalPhoto: String = ""
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            Toast.makeText(this, "11111111111111111111.", Toast.LENGTH_SHORT).show()
+            readExternalPhoto = android.Manifest.permission.READ_MEDIA_IMAGES
+        } else {
+            Toast.makeText(this, "222222222222222222222222222.", Toast.LENGTH_SHORT).show()
+            readExternalPhoto = android.Manifest.permission.READ_EXTERNAL_STORAGE
+        }
+        Toast.makeText(this, "33333333333333333333333333.", Toast.LENGTH_SHORT).show()
+        if(ContextCompat.checkSelfPermission(this, readExternalPhoto) == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "c.", Toast.LENGTH_SHORT).show()
+            openPhotoPicker()
+        } else {
+            Toast.makeText(this, "444444444444444444444444444444.", Toast.LENGTH_SHORT).show()
+            ActivityCompat.requestPermissions(this, arrayOf(readExternalPhoto), 100)
+        }
+    }
+
+    private fun openPhotoPicker() {
+        Toast.makeText(this, "d.", Toast.LENGTH_SHORT).show()
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type="image/*"
+        photoLauncher.launch(intent)
+        Toast.makeText(this, "e.", Toast.LENGTH_SHORT).show()
+    }
+
+    fun uploadToFirestore(photoUri: Uri){
+        val firestore = Firebase.firestore
+        val data= hashMapOf("profilePic" to photoUri.toString())
+        firestore.collection("users").document(userId!!)
+            .set(data)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Profile photo uploaded to Firestore", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Profile photo upload failed", Toast.LENGTH_SHORT).show()
+            }
+
+    }
+    fun postToFirestore(photoUrl: String){
+        Firebase.firestore.collection("users")
+            .document(userId!!)
+            .update("profilePic", photoUrl)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Profile photo updated", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Profile photo update failed", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+
+
 }
