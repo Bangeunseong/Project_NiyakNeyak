cea4878bedee81d023f4fb9690effe12a1f2ee59
Bangeunseong <bangeunseong@naver.com>
2024-06-12 14:27:44 +0900

    Fixed Bugs in RingActivity

--- app/src/main/java/com/capstone/project_niyakneyak/alarm/activity/RingActivity.kt
+++ app/src/main/java/com/capstone/project_niyakneyak/alarm/activity/RingActivity.kt
@@ -110,14 +110,16 @@ class RingActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        connectThread?.start()
+        if(bluetoothAdapter?.isEnabled == true)
+            connectThread?.start()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
         _firestore = null
-        connectThread?.disconnectSocket()
+        if(bluetoothAdapter?.isEnabled == true)
+            connectThread?.disconnectSocket()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
             setShowWhenLocked(false)
